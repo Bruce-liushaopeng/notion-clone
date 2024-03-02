08e18b4b931fa852dd84e4b08907617f4f1ccb35
bruceliu <bruceliu1107@gmail.com>
2024-03-02 16:11:00 -0500

    theme toggle button

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -11,7 +11,8 @@ const font = Poppins({
 const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-2">
-      <Image src="logo.svg" height="40" width="40" alt="logo" />
+      <Image src="logo.svg" height="40" width="40" alt="logo" className="dark:hidden" />
+      <Image src="logo-dark.svg" height="40" width="40" alt="logo" className="hidden dark:block" />
       <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
