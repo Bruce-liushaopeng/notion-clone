6ceeb99db1c22d7947f6132fe7779ff998d46917
bruceliu <bruceliu1107@gmail.com>
2024-03-02 12:45:54 -0500

    footer with policy and conditions

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -12,6 +12,7 @@ const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-2">
       <Image src="logo.svg" height="40" width="40" alt="logo" />
+      <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
 };
